a4547f22fdf0dac2a5f49b8642742e8c41beae77
Sanika19 <51454918+Sanika19@users.noreply.github.com>
2021-04-07 18:31:47 +0530

    Update forms.py

--- users/forms.py
+++ users/forms.py
@@ -5,10 +5,9 @@ from django.contrib.auth.forms import UserCreationForm
 class UserRegisterForm(UserCreationForm):
     email= forms.EmailField()
 
-    #class Meta gives us a nested namespace for configurations and keeps them in one place
-    #within the configuration we are stating that the model that will be affected is the user model
-    #form.save() will save it to the user model and the fields are in sequence with how we want the form to be
-
+    #class Meta gives us a nested namespace for configurations and keeps them in one place.
+    #within the configurations we are stating that the model that will be affected is the user model.
+    
     class Meta:
         model = User
         fields = ['username','email','password1','password2']
